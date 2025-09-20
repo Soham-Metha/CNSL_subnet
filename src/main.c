522fcd20e70ce6eeecedcd7fb5145efb1aee0f27
Soham Metha <sohammetha01@gmail.com>
2025-09-20 12:15:17 +0000

    20/09/2025 : 17:45:17

--- src/main.c
+++ src/main.c
@@ -42,10 +42,10 @@ int main()
     //===========================================================================================
     printf("\n┌────────────────────────────────────────────────────────────────────────┐");
     printf("\n│ IP           │  %-344s │", ip_to_str(ip, class.mask_start_at, 0));
-    printf("\n│ MASK         │  %-55s │", ip_to_str(mask, class.mask_start_at, 0));
+    printf("\n│ MASK         │  %-344s │", ip_to_str(mask, class.mask_start_at, 0));
     printf("\n│ CLASS        │  %-54s │", class.class_name);
     printf("\n├────────────────────────────────────────────────────────────────────────┤");
-    printf("\n│ SUBNET MASK  │  %-55s │", ip_to_str(sub_mask, class.mask_start_at, bit_cnt));
+    printf("\n│ SUBNET MASK  │  %-344s │", ip_to_str(sub_mask, class.mask_start_at, bit_cnt));
     printf("\n│ RANGE        │  %-54d │", cnt);
     printf("\n└────────────────────────────────────────────────────────────────────────┘\n");
 
@@ -54,8 +54,8 @@ int main()
     for (int i = 1; i <= sub_cnt; i++) {
         printf("\n│              │  %-54s │", "");
         printf("\n│ SUBNET NO.   │  %-54d │", i);
-        printf("\n│ SUBNET START │  %-54s │", ip_to_str(subnet_strt, class.mask_start_at, bit_cnt));
-        printf("\n│ SUBNET END   │  %-54s │", ip_to_str(subnet_end, class.mask_start_at, bit_cnt));
+        printf("\n│ SUBNET START │  %-344s │", ip_to_str(subnet_strt, class.mask_start_at, bit_cnt));
+        printf("\n│ SUBNET END   │  %-344s │", ip_to_str(subnet_end, class.mask_start_at, bit_cnt));
         subnet_strt.as_int += cnt;
         subnet_end.as_int += cnt;
     }
