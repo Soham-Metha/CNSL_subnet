1926f3bb223885be1b23d95840f6b6723b73f488
Soham Metha <sohammetha01@gmail.com>
2025-09-20 13:37:56 +0000

    20/09/2025 : 19:07:56

--- src/main.c
+++ src/main.c
@@ -8,52 +8,56 @@ int main()
     //===========================================================================================
     printf("Enter IP: ");
     IP ip           = { 0 };
+    IP mask         = { 0 };
+    IP_Class class  = { 0 };
     int octets_read = scanf("%hhu.%hhu.%hhu.%hhu", &ip.octet[3], &ip.octet[2], &ip.octet[1], &ip.octet[0]);
     if (octets_read < 4) {
         printf("WARN: Only read %d octet(s), defaulted remining octet(s) to 0.\n", octets_read);
     }
 
-    IP_Class class = lookup(ip);
-    IP mask        = { 0 };
+    class = lookup(ip);
     SET_BITS(mask.addr, class.nw_addr_lsb, IP_ADDRESS_SIZE - 1);
 
     //===========================================================================================
     printf("Enter Subnet Count: ");
-    unsigned char sub_cnt = 0;
-    bool sub_cnt_read     = scanf("%hhu", &sub_cnt);
-    if (sub_cnt_read != true) {
+    unsigned char subnet_cnt    = 0;
+    unsigned char subnet_bitcnt = 0;
+    unsigned char subnet_lsb    = 0;
+    IP subnet_mask              = { 0 };
+    bool subnet_cnt_read        = scanf("%hhu", &subnet_cnt);
+    if (subnet_cnt_read != true) {
         printf("WARN: Subnet count not entered, defaulted to 0.\n");
     }
 
-    unsigned char subnet_bit_cnt = get_bit_cnt(sub_cnt);
-    unsigned char subnet_lsb     = class.nw_addr_lsb - subnet_bit_cnt;
-    IP sub_mask                  = { 0 };
-    SET_BITS(sub_mask.addr, subnet_lsb, IP_ADDRESS_SIZE - 1);
+    subnet_bitcnt = get_bit_cnt(subnet_cnt);
+    subnet_lsb    = class.nw_addr_lsb - subnet_bitcnt;
+
+    SET_BITS(subnet_mask.addr, subnet_lsb, IP_ADDRESS_SIZE - 1);
 
     //===========================================================================================
     int range      = 1 << subnet_lsb;
-    IP subnet_strt = { .addr = ip.addr & sub_mask.addr };
-    IP subnet_end  = { .addr = ip.addr & sub_mask.addr };
+    IP subnet_strt = { .addr = ip.addr & subnet_mask.addr };
+    IP subnet_end  = { .addr = ip.addr & subnet_mask.addr };
     SET_BITS(subnet_end.addr, 0, subnet_lsb - 1);
 
     //===========================================================================================
     printf("\n┌────────────────────────────────────────────────────────────────────────┐");
-    printf("\n│ IP           │  %-344s │", ip_to_str(ip, class.nw_addr_lsb, 0));
+    printf("\n│ IP           │  %-244s │", ip_to_str(ip, class.nw_addr_lsb, 0));
     printf("\n│ MASK         │  %-344s │", ip_to_str(mask, class.nw_addr_lsb, 0));
     printf("\n│ CLASS        │  %-53s  │", class.name);
     printf("\n├────────────────────────────────────────────────────────────────────────┤");
-    printf("\n│ SUBNET MASK  │  %-344s │", ip_to_str(sub_mask, class.nw_addr_lsb, subnet_bit_cnt));
-    printf("\n│ SUBNET BITS  │  %-53d  │", subnet_bit_cnt);
+    printf("\n│ SUBNET MASK  │  %-344s │", ip_to_str(subnet_mask, class.nw_addr_lsb, subnet_bitcnt));
+    printf("\n│ SUBNET BITS  │  %-53d  │", subnet_bitcnt);
     printf("\n│ RANGE        │  %-53d  │", range);
     printf("\n└────────────────────────────────────────────────────────────────────────┘\n");
 
     //===========================================================================================
     printf("\n┌────────────────────────────────────────────────────────────────────────┐");
-    for (int i = 1; i <= sub_cnt; i++) {
+    for (int i = 1; i <= subnet_cnt; i++) {
         printf("\n│              │  %-53s  │", "");
         printf("\n│ SUBNET NO.   │  %-53d  │", i);
-        printf("\n│ SUBNET START │  %-344s │", ip_to_str(subnet_strt, class.nw_addr_lsb, subnet_bit_cnt));
-        printf("\n│ SUBNET END   │  %-344s │", ip_to_str(subnet_end, class.nw_addr_lsb, subnet_bit_cnt));
+        printf("\n│ SUBNET START │  %-344s │", ip_to_str(subnet_strt, class.nw_addr_lsb, subnet_bitcnt));
+        printf("\n│ SUBNET END   │  %-344s │", ip_to_str(subnet_end, class.nw_addr_lsb, subnet_bitcnt));
         subnet_strt.addr += range;
         subnet_end.addr += range;
     }
