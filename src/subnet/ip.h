27834338040ac0b838e28ae8d7bdae461cb57182
Soham Metha <sohammetha01@gmail.com>
2025-09-20 08:51:40 +0000

    20/09/2025 : 14:21:40

--- src/subnet/ip.h
+++ src/subnet/ip.h
@@ -1,8 +1,25 @@
 #ifndef PROJ
 #define PROJ
 
-#include <stdio.h>
+typedef enum IP_classes {
+    CLASS_A,
+    CLASS_B,
+    CLASS_C,
+    CLASS_D,
+    CLASS_E,
+    CLASS_CNT,
+} IP_classes;
 
-void printHello();
+typedef struct IP_Class IP_Class;
+typedef union IP IP;
+
+union IP {
+    char octet[4];
+};
+
+struct IP_Class {
+    const char* class_name;
+    int start_at;
+};
 
 #endif
