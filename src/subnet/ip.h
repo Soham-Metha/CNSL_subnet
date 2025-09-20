6ef46fed41664c456e9b206aec660ee915d610f1
Soham Metha <sohammetha01@gmail.com>
2025-09-20 09:31:09 +0000

    20/09/2025 : 15:01:09

--- src/subnet/ip.h
+++ src/subnet/ip.h
@@ -2,17 +2,17 @@
 #define PROJ
 #include <assert.h>
 
-#define SET_BIT(val, pos)             \
-    do {                              \
-        assert(pos >= 0 && pos < 32); \
-        val |= (1 << pos);            \
+#define SET_BIT(val, pos)                 \
+    do {                                  \
+        assert((pos) >= 0 && (pos) < 32); \
+        (val) |= (1U << (pos));           \
     } while (0)
 
-#define SET_BITS(val, start, stop)            \
-    do {                                      \
-        for (int i = start; i <= stop; i++) { \
-            SET_BIT(val, i)                   \
-        }                                     \
+#define SET_BITS(val, start, stop)                \
+    do {                                          \
+        for (int i = (start); i <= (stop); i++) { \
+            SET_BIT(val, i);                      \
+        }                                         \
     } while (0)
 
 typedef enum IP_classes {
