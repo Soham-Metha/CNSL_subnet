ca18d20c326daefa05a52300e436d2f5be522799
Soham Metha <sohammetha01@gmail.com>
2025-09-20 08:59:47 +0000

    20/09/2025 : 14:29:47

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@ int main()
     int cnt = scanf("%hhu.%hhu.%hhu.%hhu", &ip.octet[0], &ip.octet[1], &ip.octet[2], &ip.octet[3]);
 
     if (cnt < 4) {
-        printf("ERROR: Only read %d octets!\n", cnt);
+        printf("WARN: Only read %d octets!\n", cnt);
     }
     IP_Class class = lookup(ip);
     printf("%d %s\n", class.start_at, class.class_name);
