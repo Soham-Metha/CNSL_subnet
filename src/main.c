66b9c2ddcb85448763e710ea317569c4f12eb095
Soham Metha <sohammetha01@gmail.com>
2025-09-20 08:56:39 +0000

    20/09/2025 : 14:26:39

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@ int main()
 {
     printf("Enter IP:");
     IP ip;
-    scanf("%hhd.%hhd.%hhd.%hhd", &ip.octet[0], &ip.octet[1], &ip.octet[2], &ip.octet[3]);
+    scanf("%hhu.%hhu.%hhu.%hhu", &ip.octet[0], &ip.octet[1], &ip.octet[2], &ip.octet[3]);
     IP_Class class = lookup(ip);
     printf("%d %s\n", class, class.class_name);
 
