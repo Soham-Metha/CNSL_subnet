64d5c5257447065dc03ee261c965f84d6cfd335a
Soham Metha <sohammetha01@gmail.com>
2025-09-20 08:52:17 +0000

    20/09/2025 : 14:22:17

--- src/subnet/ip.c
+++ src/subnet/ip.c
@@ -1,6 +1,19 @@
+#include <stdio.h>
 #include <subnet/ip.h>
 
-void printHello()
+static IP_Class IP_LUT[CLASS_CNT] = {
+    [CLASS_E] = { .class_name = "CLASS E", .start_at = 248 },
+    [CLASS_D] = { .class_name = "CLASS D", .start_at = 223 },
+    [CLASS_C] = { .class_name = "CLASS C", .start_at = 192 },
+    [CLASS_B] = { .class_name = "CLASS B", .start_at = 128 },
+    [CLASS_A] = { .class_name = "CLASS A", .start_at = 0   },
+};
+
+IP_classes lookup(IP ip)
 {
-	printf("\nHello World!\n\n");
+    for (int i = CLASS_E; i > -1; i--) {
+        if (ip.octet[0] >= IP_LUT[i].start_at) {
+            return i;
+        }
+    }
 }
