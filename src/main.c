968184d32a8416d51d9f6ae2572ef7fde071c005
Soham Metha <sohammetha01@gmail.com>
2025-09-20 09:38:43 +0000

    20/09/2025 : 15:08:43

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@ int main()
     printf("Enter IP:");
     IP ip          = { 0 };
     IP mask        = { 0 };
-    int cnt        = scanf("%hhu.%hhu.%hhu.%hhu", &ip.octet[0], &ip.octet[1], &ip.octet[2], &ip.octet[3]);
+    int cnt        = scanf("%hhu.%hhu.%hhu.%hhu", &ip.octet[3], &ip.octet[2], &ip.octet[1], &ip.octet[0]);
     IP_Class class = lookup(ip);
 
     SET_BITS(mask.as_int, class.mask_start_at, 31);
@@ -15,8 +15,8 @@ int main()
         printf("\nWARN: Only read %d octet(s), defaulted remining octets to 0.\n", cnt);
     }
     printf("\n----------------------------------------");
-    printf("\nIP    |  %hhu.%hhu.%hhu.%hhu            ", ip.octet[0], ip.octet[1], ip.octet[2], ip.octet[3]);
-    printf("\nMASK  |  %hhu.%hhu.%hhu.%hhu            ", mask.octet[0], mask.octet[1], mask.octet[2], mask.octet[3]);
+    printf("\nIP    |  %hhu.%hhu.%hhu.%hhu            ", ip.octet[3], ip.octet[2], ip.octet[1], ip.octet[0]);
+    printf("\nMASK  |  %hhu.%hhu.%hhu.%hhu            ", mask.octet[3], mask.octet[2], mask.octet[1], mask.octet[0]);
     printf("\nCLASS |  %s                             ", class.class_name);
     printf("\n----------------------------------------\n\n");
     printf("%d\n", mask.as_int);
