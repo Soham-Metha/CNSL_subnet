91087b810ed6e5b1af6a9e4d32d6d7281a653af8
Soham Metha <sohammetha01@gmail.com>
2025-09-20 08:59:29 +0000

    20/09/2025 : 14:29:29

--- src/main.c
+++ src/main.c
@@ -4,8 +4,12 @@
 int main()
 {
     printf("Enter IP:");
-    IP ip;
-    (void)scanf("%hhu.%hhu.%hhu.%hhu", &ip.octet[0], &ip.octet[1], &ip.octet[2], &ip.octet[3]);
+    IP ip   = { 0 };
+    int cnt = scanf("%hhu.%hhu.%hhu.%hhu", &ip.octet[0], &ip.octet[1], &ip.octet[2], &ip.octet[3]);
+
+    if (cnt < 4) {
+        printf("ERROR: Only read %d octets!\n", cnt);
+    }
     IP_Class class = lookup(ip);
     printf("%d %s\n", class.start_at, class.class_name);
 
