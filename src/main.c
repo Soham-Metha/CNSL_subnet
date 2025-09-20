5f5caa6fe0a5f68f861cf4422cc8de071ea0563f
Soham Metha <sohammetha01@gmail.com>
2025-09-20 09:15:20 +0000

    20/09/2025 : 14:45:20

--- src/main.c
+++ src/main.c
@@ -11,7 +11,8 @@ int main()
     if (cnt < 4) {
         printf("WARN: Only read %d octet(s), defaulted remining octets to 0.\n", cnt);
     }
-
+	printf("----------------------------------------");
+	printf("IP    |  %hhu.%hhu.%hhu.%hhu            ", &ip.octet[0], &ip.octet[1], &ip.octet[2], &ip.octet[3]);
     printf("%d %s\n", class.end_at, class.class_name);
 
     return 0;
