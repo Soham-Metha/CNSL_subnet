e57017948049f03b72180734eff23e38e9cbcf38
Soham Metha <sohammetha01@gmail.com>
2025-09-20 14:32:30 +0000

    20/09/2025 : 20:02:30

--- src/subnet/ip.h
+++ src/subnet/ip.h
@@ -56,8 +56,8 @@ struct IP {
     Subnet_Range range;
 };
 
-IP_Class lookup(IP_addr ip);
+IP_Class lookup(const IP* ip);
 unsigned char get_bit_cnt(unsigned char subnet_cnt);
-const char* ip_to_str(IP_addr ip, unsigned char mask_start_at, unsigned char subnet_lsb);
+const char* ip_to_str(const IP* ip, unsigned char mask_start_at, unsigned char subnet_lsb);
 
 #endif
